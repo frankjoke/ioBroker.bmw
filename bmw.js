f73cc3fb62cc3251f708cc55fadb4a0cad84c884
Ingo Fischer <github@fischer-ka.de>
2018-01-02 13:41:40 +0100

    fix util.js usage and use utils.Adapter

--- bmw.js
+++ bmw.js
@@ -6,7 +6,7 @@
 // jshint node:true, esversion:6, strict:global, undef:true, unused:true
 "use strict";
 const utils = require('./lib/utils'); // Get common adapter utils
-const adapter = utils.adapter('bmw');
+const adapter = utils.Adapter('bmw');
 const MyAdapter = require('./myAdapter');
 const BMWConnectedDrive = require('./connectedDrive');
 const A = MyAdapter(adapter, main);
